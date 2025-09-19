9189c4690a660655ef4da36fd4a35322654648a0
ckanth15 <ckanthus@gmail.com>
2025-09-19 12:04:30 -0700

    Added CORS handles

--- Search_Gateway/Search Gateway/lambda_function.py
+++ Search_Gateway/Search Gateway/lambda_function.py
@@ -3,15 +3,28 @@ import search_page,json
 # Lambda execution starts here
 
 def lambda_handler(event, context):
-  
 
-    
+    # Handle CORS preflight requests
+    if event.get('httpMethod') == 'OPTIONS':
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
+                'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+            },
+            'body': ''
+        }  
+
     response_html = {
         "statusCode": 200,
         "statusDescription": "200 OK",
         "isBase64Encoded": False,
         "headers": {
-            "Content-Type": "text/html; charset=utf-8"
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "POST, GET, OPTIONS",
+            "Access-Control-Allow-Headers": "Content-Type, Authorization"
         }
     }
     response_html['body'] = search_page.searchhome()
